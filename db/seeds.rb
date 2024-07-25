ebe79c83ff6ebb22f70a54bf151cf3777fe0f8d4
bono43 <bono43@nexondv.com>
2024-07-25 17:54:52 +0700

    feat: add graphql

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+5.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
+  5.times do
+    user.posts.create(title: Faker::Lorem.paragraph(sentence_count: 5), body: Faker::Lorem.paragraph(sentence_count: 5))
+  end
+end
